673b53bd7621c87604dfa1ba4f1f65d722434817
Amir Elite <85869496+shMotaharpour@users.noreply.github.com>
2021-11-06 14:10:16 +0330

    اصلاح تابع PCA
    به دو صورت بردار ویژه و ماتریس منفرد

--- hosein-PCA/SimplePCA.py
+++ hosein-PCA/SimplePCA.py
@@ -1,15 +1,23 @@
 import numpy as np
 
 class SimplePCA():
-    def __init__(self, n_components=None):
+    def __init__(self, n_components=None, mode='eig'):
         self.n_components = n_components
-        
+        if mode in ('eig', 'svd'):
+            self.mode = mode  # eig or svd
+        else:
+            raise Exception('دو حالت eig و svd قابل قبول است')
     def fit(self, X, y=None):
         self.means = X.mean(axis=0)
         x2 = X - self.means
-        sigma = x2.T @ x2
-        vecs, self.mags, *_ = np.linalg.svd(sigma)
-        self.components_ = vecs[:, :self.n_components] if self.n_components else vecs
+        if self.mode == 'eig':
+            sigma = x2.T @ x2
+            eigs, eigs_vecs = np.linalg.eig(sigma)
+            orders = np.argsort(eigs)[::-1]
+            self.components_ = eigs_vecs[:,:self.n_components]
+        else:
+            *_, self.mags, vecs = np.linalg.svd(x2)
+            self.components_ = vecs[:self.n_components, :].T if self.n_components else vecs.T
         return self
     
     def trasform(self, X, y=None):
